76486868e754720df009fb1daa060a5b900e01f7
Alexandra Simkova <sasa.matulova@gmail.com>
2023-04-06 19:02:01 +0200

    Component Reservation created, router created

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <RouterProvider router = {router} />
     <Footer />
   </>
 );
+
+const router = createBrowserRouter ([
+  {
+      path: '/',
+      element: <Home />
+  },
+  {
+      path: '/reservation',
+      element: <Reservation />
+  }
+])
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 import './style.css';
 
 export const API_BASE_URL = 'https://apps.kodim.cz/daweb/leviexpress/api';
 
-createRoot(document.querySelector('#app')).render(<App />, );
+
+createRoot(document.querySelector('#app')).render(<App />);
