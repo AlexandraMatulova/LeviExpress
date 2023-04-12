0286f0e504d3fe7bb2e844cf19df080163c0b6bf
Alexandra Simkova <sasa.matulova@gmail.com>
2023-04-12 21:43:39 +0200

    Reservation and navigate added

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { JourneyPicker } from "../JourneyPicker";
 import { useState } from "react";
 import { JourneyDetail } from "../JourneyDetail";
+import { SelectedSeat } from "../SelectedSeat";
+import { useNavigate } from "react-router-dom";
 
 export const Home = () => {
   const [journey, setJourney] = useState(null);
@@ -10,10 +12,35 @@ export const Home = () => {
     setJourney(journey);
   };
 
+  const handleBuy = () => {
+    fetch("https://apps.kodim.cz/daweb/leviexpress/api/reservation", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        action: "create",
+        seat: journey.autoSeat,
+        journeyId: journey.journeyId,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => navigate(`/reservation/${data.results.reservationId}`));
+  };
+
+  const navigate = useNavigate();
+
   return (
     <main>
       <JourneyPicker onJourneyChange={handleJourneyChange} />
       {journey ? <JourneyDetail journey={journey} /> : null}
+      {journey ? <SelectedSeat number={journey.autoSeat} /> : null}
+
+      <div className="controls container">
+        <button className="btn btn--big" type="button" onClick={handleBuy}>
+          Rezervovat
+        </button>
+      </div>
     </main>
   );
 };
